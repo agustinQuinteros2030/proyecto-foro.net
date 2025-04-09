bc4a22bb2751a5d0c3850d24632c3e8402ce3bde
agustinQuinteros2030 <agustinquinteros84@gmail.com>
2025-04-08 22:29:19 -0300

    creacion de proyecto

---
+++ foro_C/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace foro_C.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
