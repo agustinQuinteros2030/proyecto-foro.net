b844ea5838bb91e879fa643efaa65bd9a58b0e0d
agustinQuinteros2030 <agustinquinteros84@gmail.com>
2025-04-11 14:09:19 -0300

    completo clase respuesta

--- foro_C/Models/Respuesta.cs
+++ foro_C/Models/Respuesta.cs
@@ -2,5 +2,10 @@
 {
     public class Respuesta
     {
+        public DateTime Fecha {  get; private set; }
+        public String Texto { get;  private set; }
+        public Persona Miembro { get;  private set; }
+          public Pregunta PreguntaQueResponde {  get; private set; }
+        public Reaccion Megusta { get; set; }
     }
 }
