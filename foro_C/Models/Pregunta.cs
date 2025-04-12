f112fcaa18b84de4a25ba92c9300068ca9c27be5
agustinQuinteros2030 <agustinquinteros84@gmail.com>
2025-04-11 23:26:49 -0300

    arreglo de sintaxis en la clase pregunta

--- foro_C/Models/Pregunta.cs
+++ foro_C/Models/Pregunta.cs
@@ -8,8 +8,8 @@ namespace foro_C.Models
         public DateTime Fecha { get; private set; }
         public String Texto { get; private set; }
         public Miembro Miembro { get; private set; }
-        public Bool Activa { get; private set; }
-        public Entrada Entrada { get; private set }
+        public Boolean Activa { get; private set; }
+        public Entrada Entrada { get; private set; }
         public List<Respuesta> Respuestas { get; private set; }
 
 
