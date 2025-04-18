edcc893d1baf9e877b89de257c19099ebf4d35dd
agustinQuinteros2030 <agustinquinteros84@gmail.com>
2025-04-18 10:58:59 -0300

    cambios en la clase Persona y dire

--- foro_C/Models/Direccion.cs
+++ foro_C/Models/Direccion.cs
@@ -4,12 +4,13 @@ namespace foro_C.Models
 {
     public class Direccion
     {
+        public int Id{ get; set; }
         public String Calle { get;  private set; }
         public String Numero { get; private set; }
         //prop navegacional
       public Persona persona { get;  private set; }
         //prop relacional
-      public int PersonaID { get; private set; }
+     
       
     }
 }
