aef754f76918e48e558a32772c40ea797a6c450b
agustinQuinteros2030 <agustinquinteros84@gmail.com>
2025-05-09 21:06:55 -0300

    creacion de la clase ForoContext: y de las propiedades

---
+++ foro_C/Data/ForoContext.cs
@@ -0,0 +1,23 @@
+﻿using foro_C.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace foro_C.Data
+{
+    public class ForoContext:DbContext
+    {
+        public ForoContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Miembro> Miembros { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Entrada> Entradas { get; set; }
+        public DbSet<Pregunta> Preguntas { get; set; }
+        public DbSet<Respuesta> Respuestas { get; set; }
+        public DbSet<Reaccion> Reacciones { get; set; }
+        public DbSet<Habilitacion> Habilitaciones { get; set; }
+        public DbSet<Direccion> Direcciones { get; set; }
+    }
+}
