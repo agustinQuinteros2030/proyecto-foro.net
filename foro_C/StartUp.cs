e17cd4cf30820345d5cb8ba17d44b8c7a31bc339
agustinQuinteros2030 <agustinquinteros84@gmail.com>
2025-05-12 17:48:38 -0300

    configuramos la clase StartUp

---
+++ foro_C/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace foro_C
+{
+    public static class StartUp
+    {
+        public static WebApplication inicializarApp (String[] args)
+        {
+            //creamos instancia de nuestro servidor web
+            var builder = WebApplication.CreateBuilder(args);
+
+            ConfiguresServices(builder);//lo configuramos con sus servicios 
+            var app = builder.Build();//despues configuramos los middelware 
+            Configure(app);
+
+            return app;
+        }
+
+      private static void ConfiguresServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllersWithViews();
+        }
+
+      private static void Configure(WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+}
