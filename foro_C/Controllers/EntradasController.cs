e151d66c02f26d7ad29d643c11d4360309818fae
agustinQuinteros2030 <agustinquinteros84@gmail.com>
2025-05-22 14:44:35 -0300

    agrego todo el scafolding

---
+++ foro_C/Controllers/EntradasController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using foro_C.Data;
+using foro_C.Models;
+
+namespace foro_C.Controllers
+{
+    public class EntradasController : Controller
+    {
+        private readonly ForoContext _context;
+
+        public EntradasController(ForoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Entradas
+        public async Task<IActionResult> Index()
+        {
+            var foroContext = _context.Entradas.Include(e => e.Categoria).Include(e => e.Miembro);
+            return View(await foroContext.ToListAsync());
+        }
+
+        // GET: Entradas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entrada = await _context.Entradas
+                .Include(e => e.Categoria)
+                .Include(e => e.Miembro)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (entrada == null)
+            {
+                return NotFound();
+            }
+
+            return View(entrada);
+        }
+
+        // GET: Entradas/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Nombre");
+            ViewData["MiembroId"] = new SelectList(_context.Miembros, "Id", "Apellido");
+            return View();
+        }
+
+        // POST: Entradas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Fecha,Titulo,Texto,Privada,CategoriaId,MiembroId")] Entrada entrada)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(entrada);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Nombre", entrada.CategoriaId);
+            ViewData["MiembroId"] = new SelectList(_context.Miembros, "Id", "Apellido", entrada.MiembroId);
+            return View(entrada);
+        }
+
+        // GET: Entradas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entrada = await _context.Entradas.FindAsync(id);
+            if (entrada == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Nombre", entrada.CategoriaId);
+            ViewData["MiembroId"] = new SelectList(_context.Miembros, "Id", "Apellido", entrada.MiembroId);
+            return View(entrada);
+        }
+
+        // POST: Entradas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Fecha,Titulo,Texto,Privada,CategoriaId,MiembroId")] Entrada entrada)
+        {
+            if (id != entrada.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(entrada);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EntradaExists(entrada.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Nombre", entrada.CategoriaId);
+            ViewData["MiembroId"] = new SelectList(_context.Miembros, "Id", "Apellido", entrada.MiembroId);
+            return View(entrada);
+        }
+
+        // GET: Entradas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entrada = await _context.Entradas
+                .Include(e => e.Categoria)
+                .Include(e => e.Miembro)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (entrada == null)
+            {
+                return NotFound();
+            }
+
+            return View(entrada);
+        }
+
+        // POST: Entradas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var entrada = await _context.Entradas.FindAsync(id);
+            if (entrada != null)
+            {
+                _context.Entradas.Remove(entrada);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EntradaExists(int id)
+        {
+            return _context.Entradas.Any(e => e.Id == id);
+        }
+    }
+}
