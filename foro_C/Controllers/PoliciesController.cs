86cad1806a3c21bb773f0f5e1a9a01ddf4522f2d
lucianapradenas <lucianapradenas3@gmail.com>
2025-06-10 21:57:29 -0300

    cree las vistas de las politicas de la pagina

---
+++ foro_C/Controllers/PoliciesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace foro_C.Controllers
+{
+    public class PoliciesController : Controller
+    {
+        public IActionResult Rules()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult UserAgreement()
+        {
+            return View();
+        }
+    }
+}
+
